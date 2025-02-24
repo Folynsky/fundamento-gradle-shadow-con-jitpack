58a9f2d5fe3bd65b3068c4e0f8c8818cdcf475e0
Foly <kevin.foly06@gmail.com>
2025-02-24 13:26:46 -0600

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import com.soundicly.jnanoidenhanced.jnanoid.NanoIdUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        String id = NanoIdUtils.randomNanoId();
+        System.out.println("Hello, World!");
+        System.out.println(id);
+    }
+}
